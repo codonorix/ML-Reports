4c86e3ce9c9931ce08a832a0d890d1f04aff06cd
codonorix <78849453+codonorix@users.noreply.github.com>
2022-08-30 13:15:56 +0200

    Add files via upload

---
+++ ml-report/src/main/java/za/co/codonorix/mlreport/MlReport.java
@@ -0,0 +1,17 @@
+package za.co.codonorix.mlreport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MlReport extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
